5d6390b74241a24a1d1730dc963d60bebe0cd043
cesarandreslopez <cesarandreslopez@gmail.com>
2019-06-20 19:00:11 +0400

    download the original weights for training preparation

--- prepareandtest.sh
+++ prepareandtest.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env sh
 cd /floyd/home/darknet
 wget https://pjreddie.com/media/files/yolov3.weights
+wget https://pjreddie.com/media/files/darknet53.conv.74
 make
 ./darknet detect cfg/yolov3.cfg yolov3.weights data/dog.jpg
